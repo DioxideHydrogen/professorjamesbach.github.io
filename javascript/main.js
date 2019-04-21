fabf20ae6679d15af91846371318582f79d90f18
ProfessorJamesBach <mxhugoxm@gmail.com>
2019-04-21 00:05:05 -0300

    hugohenrique.github.io

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+//Função para retonar ao início
+function linkInicio(){
+    //Redirecionamento
+    window.location.href = "index.html";
+}
+
+//Função para ir aos portifólios de Hugo Henrique
+function linkPortifolios(){
+    //Redirecionamento
+    window.location.href = "portifolios.html";
+}
+
+//Função para ir aos contatos de Hugo Henrique
+function linkContate(){
+    //Redirecionamento
+    window.location.href = "contate.html";
+}
+
